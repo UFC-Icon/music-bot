98beb67279177ff8cddf62f81a7c51800a0bd493
UFC-Icon <tlcbc@hi2.in>
2026-02-08 18:13:47 +0530

    Final fix for Koyeb deployment

--- bot.py
+++ bot.py
@@ -1,3 +1,20 @@
 import os
-TOKEN = os.getenv("8338276223:AAFGkuo6r2Bk_1e935QSqblVXmaOccfg-c8")
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("8338276223:AAGwQPZVADquEJePHakTUENdVii_LiPVjCM")
+
+if not TOKEN:
+    print("TOKEN not found! Add it in Koyeb Environment Variables.")
+    exit()
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot is alive and running 24/7 😤🔥")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+print("Bot started successfully...")
+
+app.run_polling()
 
